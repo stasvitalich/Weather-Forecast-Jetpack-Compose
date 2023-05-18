fc8af2d9f73cf1202bc92922e999bef28caf4dba
stasvitalich <stasvitalich1994@gmail.com>
2023-05-18 21:13:52 +0600

    Basic layout was created. Column was added, boxes, text and button.

--- app/src/main/java/com/example/weatherforecast/MainActivity.kt
+++ app/src/main/java/com/example/weatherforecast/MainActivity.kt
@@ -3,13 +3,18 @@ package com.example.weatherforecast
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.weatherforecast.ui.theme.WeatherForecastTheme
 
 class MainActivity : ComponentActivity() {
@@ -31,7 +36,32 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+    Column(modifier = Modifier.fillMaxSize()) {
+
+        Box(
+            modifier = Modifier
+                .fillMaxHeight(0.5f)
+                .fillMaxWidth()
+                .background(color = Color.Green),
+            contentAlignment = Alignment.Center
+        ) {
+            Text(text = "Temperature in $name")
+        }
+
+        Box(
+            modifier = Modifier
+                .fillMaxHeight()
+                .fillMaxWidth()
+                .background(color = Color.LightGray),
+            contentAlignment = Alignment.BottomCenter
+        ) {
+            Button(onClick = { /*TODO*/ }, modifier = Modifier.padding(10.dp).fillMaxWidth()) {
+                Text(text = "Get the result")
+            }
+
+        }
+
+    }
 }
 
 @Preview(showBackground = true)
