9a40028cfb436732c0af3f70e5247d7c83f24ab0
stasvitalich <stasvitalich1994@gmail.com>
2023-05-19 15:29:40 +0600

    volley library was added for simple network requests. openweathermap was implemented.

--- app/src/main/java/com/example/weatherforecast/MainActivity.kt
+++ app/src/main/java/com/example/weatherforecast/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.weatherforecast
 
+import android.content.Context
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -10,12 +11,22 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
 import com.example.weatherforecast.ui.theme.WeatherForecastTheme
+import org.json.JSONObject
+
+const val API_KEY = "98fe4fad6e8eb7aaf6b1942c9bc6615e"
+val latitude = 52.235001
+val longitude = 76.374451
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -27,7 +38,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    Greeting("Pavlodar", this)
                 }
             }
         }
@@ -35,7 +46,22 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
+fun Greeting(name: String, context: Context) {
+
+    val state = remember {
+        mutableStateOf("Unknown")
+    }
+
+    val temperatureCel = remember {
+        mutableStateOf(0.0)
+    }
+
+    val roundedTemperature = remember {
+        mutableStateOf("0.0")
+    }
+
+
+
     Column(modifier = Modifier.fillMaxSize()) {
 
         Box(
@@ -45,7 +71,8 @@ fun Greeting(name: String) {
                 .background(color = Color.Green),
             contentAlignment = Alignment.Center
         ) {
-            Text(text = "Temperature in $name")
+
+            Text(text = "Temperature in $name = ${roundedTemperature.value}")
         }
 
         Box(
@@ -55,7 +82,13 @@ fun Greeting(name: String) {
                 .background(color = Color.LightGray),
             contentAlignment = Alignment.BottomCenter
         ) {
-            Button(onClick = { /*TODO*/ }, modifier = Modifier.padding(10.dp).fillMaxWidth()) {
+            Button(
+                onClick = {
+                    getResult("Pavlodar", state, temperatureCel, roundedTemperature, context)
+                }, modifier = Modifier
+                    .padding(10.dp)
+                    .fillMaxWidth()
+            ) {
                 Text(text = "Get the result")
             }
 
@@ -64,10 +97,32 @@ fun Greeting(name: String) {
     }
 }
 
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    WeatherForecastTheme {
-        Greeting("Android")
-    }
+private fun getResult(
+    city: String,
+    state: MutableState<String>,
+    temperatureCel: MutableState<Double>,
+    roundedTemperature: MutableState<String>,
+    context: Context
+) {
+
+    val url =
+        "https://api.openweathermap.org/data/2.5/weather?lat=$latitude&lon=$longitude&appid=$API_KEY"
+
+    val queue = Volley.newRequestQueue(context)
+    val stringRequest = StringRequest(
+        com.android.volley.Request.Method.GET,
+        url,
+        { response ->
+            val obj = JSONObject(response)
+            state.value = obj.getJSONObject("main").getString("temp")
+            temperatureCel.value = state.value.toDouble() - 273.15
+            roundedTemperature.value = "%.1f".format(temperatureCel.value)
+
+        },
+        { error ->
+        }
+
+    )
+    queue.add(stringRequest)
+
 }
